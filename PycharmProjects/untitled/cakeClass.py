e9da64dc2ad2cb63faff3b22ff1035c96bcf96b7
kwiznia <kwiznia@gmail.com>
2015-01-21 17:34:57 +0100

    Delete cakeClass.py

--- PycharmProjects/untitled/cakeClass.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'kwiznia'
-from selenium import webdriver
-
-class url():
-    driver = webdriver.Firefox()
-    driver.get("http://www.cheesecake.com/Cakes-9-Inch.asp")
-    driver.implicitly_wait(10)
