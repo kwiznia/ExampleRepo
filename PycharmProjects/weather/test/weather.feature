68cf03e4b02518f21768213b285789550cfd014a
Karen Wiznia <kwiznia@gmail.com>
2015-02-02 17:06:02 +0100

    new .py and .feature files added using requests (with blank lines at the end)

---
+++ PycharmProjects/weather/test/weather.feature
@@ -0,0 +1,8 @@
+Feature: Weather temperature
+    In order to play with Lettuce
+    As beginners
+    We'll get the temperature of a city
+
+Scenario: Main Page works
+    Given I access the url
+    Then I get the temperature in centigrade
---
+++ PycharmProjects/weather/test/weather.py
@@ -0,0 +1,11 @@
+__author__ = 'kwiznia'
+import requests
+
+class weather():
+
+    def openpage(self):
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk')
+        return r
+
+    def gettemperature(self):
+        print(self.openpage().text)
