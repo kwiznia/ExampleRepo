79a6a0920e1befb1451635b824385aab952a5b94
Karen Wiznia <kwiznia@gmail.com>
2015-02-17 12:04:49 +0100

    add new feature

--- PycharmProjects/weather2/test/weather2.py
+++ PycharmProjects/weather2/test/weather2.py
@@ -4,15 +4,15 @@ from lettuce import world
 import json
 
 class weather2():
-
+    """
     def getStatus_code(self, response):
         world.response = response
         world.response = requests.get(response)
         return world.response.status_code
+    """
 
     def getInformation(self, response):
         world.response = response
         world.response = requests.get(response)
-        #print(world.response.json())
         return world.response.json()
 		
